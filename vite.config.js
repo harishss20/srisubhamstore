94b3c62029f57f551cf713fddc153446571d06ca
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-12-23 12:31:04 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// Define external modules
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["module-name"], // Replace 'module-name' with the actual module causing the issue
-    },
-  },
+  base: "/srisubhamstore/",
 });
