271459fa472208351e222090064fe9e590c4e98d
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-12-23 12:39:18 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// Define the Vite configuration
 export default defineConfig({
   plugins: [react()],
   base: "/srisubhamstore/", // Make sure this matches your GitHub repository name
   build: {
-    outDir: "dist", // The output directory for the production build
+    rollupOptions: {
+      external: [
+        "slick-carousel/slick/slick.css",
+        "slick-carousel/slick/slick-theme.css",
+      ],
+    },
   },
 });
