4201cb0d35ac055f59bfb5195a6654ea9ac1feb4
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-12-23 12:35:14 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "slick-carousel/slick/slick-theme.css";
 import Brands from "./components/Brands";
 import Services from "./components/Services";
 import Footer from "./components/Footer";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// Define the Vite configuration
 export default defineConfig({
   plugins: [react()],
-  base: "/srisubhamstore/",
+  base: "/srisubhamstore/", // Make sure this matches your GitHub repository name
+  build: {
+    outDir: "dist", // The output directory for the production build
+  },
 });
