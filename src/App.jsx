83c6de3de0a3a662f02ec241f3137e67dc1e21fa
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-12-24 07:46:49 +0530

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import "slick-carousel/slick/slick-theme.css";
 import Brands from "./components/Brands";
 import Services from "./components/Services";
 import Footer from "./components/Footer";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -8,21 +8,24 @@ import ErrorPage from "./components/ErrorPage.jsx";
 import ProductsList from "./components/ProductsList.jsx";
 import About from "./components/About.jsx";
 import Policy from "./components/Policy.jsx";
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+    },
+    { path: "/privacy", element: <Policy />, errorElement: <ErrorPage /> },
+    { path: "/about", element: <About />, errorElement: <ErrorPage /> },
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  { path: "/privacy", element: <Policy />, errorElement: <ErrorPage /> },
-  { path: "/about", element: <About />, errorElement: <ErrorPage /> },
-  {
-    path: "/productsList",
-    element: <ProductsList />,
-    errorElement: <ErrorPage />,
-  },
-]);
+    {
+      path: "/productsList",
+      element: <ProductsList />,
+      errorElement: <ErrorPage />,
+    },
+  ],
+  { basename: "srisubhamstore" }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
